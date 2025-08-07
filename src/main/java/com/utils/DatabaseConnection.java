f8285a5885f0a148243b7e27d6572e3d5aa89635
Muhammed-Adnaan <muhammedadnaan33@gmail.com>
2025-08-07 13:32:25 +0530

    added Sql as database connection

--- src/main/java/com/utils/DatabaseConnection.java
+++ src/main/java/com/utils/DatabaseConnection.java
@@ -5,17 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-	static {
-		try {
-			// Load the SQLite JDBC driver
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("SQLite JDBC driver not found", e);
-		}
-	}
-	public static Connection getConnection() throws SQLException {
-		String url = "jdbc:sqlite:BuddyFinder.db";
-		Connection con = DriverManager.getConnection(url);
+//	static {
+//		try {
+//			// Load the SQLite JDBC driver
+//			Class.forName("org.sqlite.JDBC");
+//		} catch (ClassNotFoundException e) {
+//			throw new RuntimeException("SQLite JDBC driver not found", e);
+//		}
+//	}
+//	public static Connection getConnection() throws SQLException {
+//		String url = "jdbc:sqlite:BuddyFinder.db";
+//		Connection con = DriverManager.getConnection(url);
+//		return con;
+//	}
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		String url = "jdbc:mysql://localhost:3306";
+		String user = "root";
+		String pass = "root";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,user,pass);
 		return con;
 	}
 }
